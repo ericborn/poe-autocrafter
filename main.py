6088eda914a366ab317f571dc32052414a2a0db1
Eric Born <eric.born85@gmail.com>
2020-05-14 10:48:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,29 @@
 """
 Created on Thu May 14 09:38:23 2020
 
+Program is written to read the screen while playing the game Path of Exile
+Automatically crafts an item looking for specific mods and stops once the mods
+have been achieved
+
 @author: Eric
+
+Game played on fullscreen borderless at 1920x1080
+Coordinates to look within for text
+top left
+1000x175
+
+top right
+1600x175
+
+bottom left
+1000x640
+
+bottom right
+1600x640
 """
-from PIL import Image
+from PIL import ImageGrab
 import os
-#import io
+import cv2
 import pytesseract
 
 # setup file location
@@ -14,8 +32,13 @@ path = r'C:\Code projects\git projects\poe-autocrafter\images'
 file = 'stash1'
 full_path = os.path.join(path, file + '.png')
 
+# set path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
+# uses pytess image to string pulling from the image with 
+# language set to english
 text = pytesseract.image_to_string(full_path, lang = 'eng')
 
 print(text)
+
+
