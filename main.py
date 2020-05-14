070959e0891213c333a13e767bb2b19fd66aa8b5
Eric Born <eric.born85@gmail.com>
2020-05-14 11:19:17 -0700

    Update main.py
    Added coordinates for multiple possible item locations

--- main.py
+++ main.py
@@ -10,35 +10,45 @@ have been achieved
 
 Game played on fullscreen borderless at 1920x1080
 Coordinates to look within for text
-top left
-1000x175
 
-top right
-1600x175
+top left inventory
+(1000, 175, 1600, 640)
 
-bottom left
-1000x640
+bottom right inventory
+(1360, 500, 1920, 830)
 
-bottom right
-1600x640
+bottom middle of stash tab
+(20, 400, 580,745)
+
 """
-from PIL import ImageGrab
+from PIL import Image, ImageGrab
 import os
 import cv2
 import pytesseract
-
-# setup file location
-path = r'C:\Code projects\git projects\poe-autocrafter\images'
-file = 'stash1'
-full_path = os.path.join(path, file + '.png')
+import numpy as np
 
 # set path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
+# Manual test for a static image
+# setup file location
+#path = r'C:\Code projects\git projects\poe-autocrafter\images'
+#file = 'stash1'
+#full_path = os.path.join(path, file + '.png')
+
 # uses pytess image to string pulling from the image with 
 # language set to english
-text = pytesseract.image_to_string(full_path, lang = 'eng')
+#text = pytesseract.image_to_string(full_path, lang = 'eng')
 
-print(text)
+#print(text)
 
+screen_cap = ImageGrab.grab(bbox = (20, 400, 580,745))
+
+# used to open the image that is stored in screen_cap
+#screen_cap.show()
+
+tesstr = pytesseract.image_to_string( 
+                cv2.cvtColor(np.array(screen_cap), cv2.COLOR_BGR2GRAY),  
+                lang ='eng') 
+print(tesstr) 
 
