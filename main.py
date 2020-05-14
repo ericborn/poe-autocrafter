b97f0b5cd624f569dcb87ae6eec87824d61b77fa
Eric Born <eric.born85@gmail.com>
2020-05-14 10:32:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ Created on Thu May 14 09:38:23 2020
 @author: Eric
 """
 from PIL import Image
+import os
+#import io
 import pytesseract
 
+# setup file location
+path = r'C:\Code projects\git projects\poe-autocrafter\images'
+file = 'stash1'
+full_path = os.path.join(path, file + '.png')
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+text = pytesseract.image_to_string(full_path, lang = 'eng')
+
+print(text)
