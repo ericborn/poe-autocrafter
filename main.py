2ae83dfe414b0dc46e505f7e7a460f91b95747f5
Eric Born <eric.born85@gmail.com>
2020-05-14 12:35:04 -0700

    Update main.py
    Add code for constrast adjustment

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ bottom middle of stash tab
 testing at windowed 800x600, game in top left corner produced worse results
 (10, 270, 320, 445)
 """
-from PIL import Image, ImageGrab
+from PIL import Image, ImageGrab, ImageEnhance
 import os
 import cv2
 import pytesseract
@@ -31,8 +31,6 @@ import numpy as np
 
 # set path to tesseract.exe
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-
-15, 285
 
 # Manual test for a static image
 # setup file location
@@ -46,22 +44,57 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 
 #print(text)
 
+# magic map test in empty stash tab
 # width x height
 # 550x350 original
 # 1650x1050 x3 size
-screen_cap = ImageGrab.grab(bbox = (30, 400 , 580, 750))
+#screen_cap = ImageGrab.grab(bbox = (30, 400 , 580, 750))
 
 # used to open the image that is stored in screen_cap
 #screen_cap.show()
 
 # resizes image 3x from 550x300 to 1650x1050
-larger_image = screen_cap.resize((1650,1050))
+#larger_image = screen_cap.resize((1650,1050))
 
 # save image to file
 #larger_image.save('test1.png')
 
-tesstr = pytesseract.image_to_string(larger_image,
-                #cv2.cvtColor(np.array(screen_cap), cv2.COLOR_BGR2GRAY),  
+#tesstr = pytesseract.image_to_string(larger_image,
+                #cv2.cvtColor(np.array(larger_image), cv2.COLOR_BGR2GRAY),  
+#                lang ='eng') 
+#print(tesstr)
+
+# rare boots test in empty stash tab
+# width x height of an entire normal stash tab
+# 635x580 original
+# 1905x1740 x3 size
+screen_cap = ImageGrab.grab(bbox = (15, 170 , 650, 750))
+
+# used to open the image that is stored in screen_cap
+#screen_cap.show()
+
+# resizes image 3x from 635x580 to 1905x1740
+larger_image = screen_cap.resize((1905,1740))
+
+larger_image.show()
+
+# output of 3x image works fairly well when converting to black and white
+# correct text capture is ~90-95%
+parsed_lrg_img = pytesseract.image_to_string(
+                cv2.cvtColor(np.array(larger_image), cv2.COLOR_BGR2GRAY),  
                 lang ='eng') 
-print(tesstr) 
+print(tesstr)
+
+# creates a contrast enhancer for the larger_image
+enhancer = ImageEnhance.Contrast(larger_image)
+
+# setting factor greater than 1 increases contrast
+factor = 2
+
+enhance_output = enhancer.enhance(factor)
+
+enhance_output.show()
+
+# save image to file
+#larger_image.save('test1.png')
 
