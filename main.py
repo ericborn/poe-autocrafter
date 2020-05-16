0400f5337ec88d9ef6c65e957ab1638894084397
Eric Born <eric.born85@gmail.com>
2020-05-16 12:06:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ Scaling image by 3x produces much better results than the native resolution
 
 Enhancing contrast by 2 also improves parsing results, 1.5 and 2.5 produce
 worse results
-
 """
 from PIL import Image, ImageGrab, ImageEnhance
 import re
-import time as t
+#import time as t
 #import os
 #import cv2
 import pyautogui as gui
@@ -54,6 +53,70 @@ top header for stash and inventory text
 1920x90
 '''
 
+header_coords = (0, 0, 1920, 90)
+inventory_coords = (1270, 585, 1298, 607)
+stash_coords = (15, 170, 650, 750)
+
+# store the grey RGB value indicating its a rare item
+grey_value = (200, 200, 200)
+
+# store the yellow RGB value indicating its a rare item
+yellow_value = (254, 254, 118)
+
+# store blue RGB values
+blue_values = ((135, 135, 254), (98, 98, 188), (99, 99, 189), (74, 73, 142),\
+               (74, 74, 142), (73, 73, 141), (127, 127, 239), (81, 81, 155),\
+               (98, 98, 162), (108, 108, 178), (96, 96, 182), (125, 125, 233),\
+               (108, 108, 181), (97, 97, 184), (123, 123, 233),(109, 109, 207))
+
+# create list containing the words we're looking for in the image
+header_words = ['stash','inventory']
+
+# function takes an image as an input, creates a pixel map, iterates over the
+# pixels and changes any blue ones to white
+# helps tesseract read the text
+def color_text(img):
+    # create a pixel map from the image
+    img_pixels = img.load()
+    # loops that evaluate the stash image pixels and change any blue to white
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            for k in range(len(blue_values)):
+                # change blue pixels to white
+                if img_pixels[i,j] == blue_values[k]:
+                    img_pixels[i,j] = (255, 255, 255)
+    return(img)
+
+
+
+# Takes an image as input, multiply the dimensions by 3, 
+# converts to b/w, create enhancer, apply contrast 3 times, output list with
+# b/w and 3 contrast adjusted images.
+def image_adjustments(img):
+    
+    # resizes image 3x larger 
+    img = img.resize(((img.size[0] * 3),(img.size[1] * 3)))
+    
+    # convert enlarged image to black and white
+    img = img.convert(mode='L')                
+    
+    # setup an enhancer for the black and white image
+    img_enhancer = ImageEnhance.Contrast(img)
+    
+    # adjust contrast on the black and white imaqge
+    img_enhance_1_5 = img_enhancer.enhance(1.5)
+    img_enhance_2 = img_enhancer.enhance(2)
+    img_enhance_2_5 = img_enhancer.enhance(2.5)
+    
+    # creates a list of the four images that were created
+    stash_image_list = [img, img_enhance_1_5, img_enhance_2, \
+                  img_enhance_2_5]
+    return(stash_image_list)
+
+# Testing functions
+#stash_img = color_text(stash_img)
+#img_list = image_adjustments(header_img)
+
 # 1. screenshot top of screen
 header_img = ImageGrab.grab(bbox = (0, 0, 1920, 90))
 #header_img.show()
@@ -76,8 +139,7 @@ header_img = header_enhancer.enhance(1.5)
 # parse the headers text
 header_text = pytesseract.image_to_string(header_img, lang='eng', 
                                           config = '--psm 12').lower()
-# create list containing the words we're looking for in the image
-header_words = ['stash','inventory']
+
 
 # 2. if inventory and stash are not open, raise error and quit
 header_found = 0
@@ -129,17 +191,7 @@ stash_img = ImageGrab.grab(bbox = (15, 170, 650, 750))
 # create a pixel map from the image
 stash_img_pixels = stash_img.load()
 
-# store the grey RGB value indicating its a rare item
-grey_value = (200, 200, 200)
 
-# store the yellow RGB value indicating its a rare item
-yellow_value = (254, 254, 118)
-
-# store blue RGB values
-blue_values = ((135, 135, 254), (98, 98, 188), (99, 99, 189), (74, 73, 142),\
-               (74, 74, 142), (73, 73, 141), (127, 127, 239), (81, 81, 155),\
-               (98, 98, 162), (108, 108, 178), (96, 96, 182), (125, 125, 233),\
-               (108, 108, 181), (97, 97, 184), (123, 123, 233),(109, 109, 207))
 
 for i in range(stash_img.size[0]):
     for j in range(stash_img.size[1]):
@@ -150,42 +202,9 @@ for i in range(stash_img.size[0]):
             raise Exception('This item is normal and cannot be alted. ' \
                     'Place a magic item to be crafted.')
 
-def color_text(img):
-    # create a pixel map from the image
-    img_pixels = img.load()
-    # loops that evaluate the stash image pixels and change any blue to white
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            for k in range(len(blue_values)):
-                # change blue pixels to white
-                if img_pixels[i,j] == blue_values[k]:
-                    img_pixels[i,j] = (255, 255, 255)
-    return(img)
-
-# !!!TODO!!!
-# TEST THIS
-stash_img = color_text(stash_img)
-def image_adjustments(img):
-    # resizes image 3x from 635x580 to 1905x1740
-    img = img.resize((1905,1740))
-    
-    # convert enlarged image to black and white
-    img = img.convert(mode='L')                
-    
-    # setup an enhancer for the black and white image
-    stash_enhancer = ImageEnhance.Contrast(img)
-    
-    # adjust contrast on the black and white imaqge
-    stash_enhance_1_5 = stash_enhancer.enhance(1.5)
-    stash_enhance_2 = stash_enhancer.enhance(2)
-    stash_enhance_2_5 = stash_enhancer.enhance(2.5)
-    
-    # creates a list of the four images that were created
-    stash_image_list = [stash_img, stash_enhance_1_5, stash_enhance_2, \
-                  stash_enhance_2_5]
-    return(stash_image_list, img)
 
 
+img_list[0]
 
 # create a list which will contain the text from each parsed image
 parsed_list = []
