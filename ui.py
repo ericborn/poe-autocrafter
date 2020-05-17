4df610904b6abe3469be789166cf3f7c58371294
Eric Born <eric.born85@gmail.com>
2020-05-17 14:28:18 -0700

    Create ui.py
    starting UI creation

---
+++ ui.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17
+
+UI for the POE autocrafter
+
+@author: Eric
+"""
+import tkinter as tk
+
+# initalize window
+window = tk.Tk() 
+
+# change title
+window.title=('POE AutoCrafter')
+
+# create button
+start_button_widget = tk.Button(window)
+
+# call window
+window.mainloop()
