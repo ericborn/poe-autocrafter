e6853e89fc14a6861613f270d45601eef68ce68f
Eric Born <eric.born85@gmail.com>
2020-05-18 14:44:18 -0700

    Create find_tab.py

---
+++ find_tab.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 12:32:31 2020
+
+@author: Eric
+
+Used to find a particular tab within the inventory
+"""
+
+#!!!TODO!!!
+# create function that takes tab name as input. Clicks tab scroll button to
+# the left and screenshots. repeats until the tab names havent changed. If tab
+# not found clicks right button x times and screenshots, parses text. Continues
+# until tab is found
