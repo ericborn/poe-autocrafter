ed87bb3d7755e7a60ba3a96488b323bbec40455e
Eric Born <eric.born85@gmail.com>
2020-05-18 11:11:43 -0700

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -5,13 +5,15 @@ Created on Sun May 17 21:13:44 2020
 @author: Eric
 """
 import re
-import time
+#import time
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+###########################################################
+# Selenium
 # setup options to make chrome headless
 opts = Options()
 opts.headless = True
@@ -25,26 +27,36 @@ driver = webdriver.Chrome(r'C:/chromedriver/chromedriver.exe', options=opts)
 # loads the URL
 driver.get('https://www.pathofexile.com/trade/search/Delirium')
 
-# finds and clicks the search button
-botton = driver.find_element_by_name('searchID')
-botton.submit()
-
 # convert the page to beautiful soup format
 soup = bs(driver.page_source, 'html.parser')
 
-# selects all table data
-table = soup.findAll('div', {"class": "cbResultSetPanelDataContainer"})
+table = soup.find_all('span', attrs={'class': 'multiselect__option'})
+#table = soup.find_all('i', attrs={'class': 'mutate-type mutate-type-pseudo'})
+#table = soup.find_all('i', attrs={'class': 'mutate-type mutate-type-explicit'})
 
+mod_list = []
 
-mod_page = requests.get('https://www.pathofexile.com/trade/search/Delirium/Ab3LSL')
-mod_source = mod_page.content.decode('utf-8')
+for span in soup.select('span'):
+    mod_list.append(span.text)
 
-soup = bs(mod_source, 'lxml')
-soup.find('i', class_ = 'mutate-type mutate-type-pseudo')
-soup.find_all('span')
+# mods start at 1587 and end at 2587
+#mod_list = mod_list[1587:2587]
 
-soup.get_text
+mod_df = pd.DataFrame(data=mod_list[1587:2587])
 
-soup.find('i class')
+mod_df[]
 
-i class="mutate-type mutate-type-pseudo"
+# Selenium
+#####################################################
+# BS
+#mod_page = requests.get('https://www.pathofexile.com/trade/search/Delirium')
+#mod_source = mod_page.content.decode('utf-8')
+#
+#soup = bs(mod_source, 'lxml')
+#table = soup.find_all('i', attrs={'class': 'mutate-type mutate-type-explicit'})
+#
+#mod_list = []
+#
+#for span in soup.select('span'):
+#    mod_list.append(span.text)
+# BS
