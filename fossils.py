32b7656af99628b81c3b0f34f6686c337797706b
Eric Born <eric.born85@gmail.com>
2020-05-18 14:44:23 -0700

    Create fossils.py

---
+++ fossils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 12:30:25 2020
+
+@author: Eric
+
+Used to combine fossils and resonators
+"""
+
+#!!!TODO!!!
+# import function to find appropriate tab for fossils
+# move resonators into inventory and unstack
+# move fossils into resonators
