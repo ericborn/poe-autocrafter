85c49a3f77eaf28184c96029284698f7c78f3a91
Eric Born <eric.born85@gmail.com>
2020-05-18 14:45:44 -0700

    Create move_to_stash.py

---
+++ move_to_stash.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 12:40:21 2020
+
+@author: Eric
+
+Used to locate, move to and open stash
+"""
+
+# !!!TODO!!!
+# write code to screencap top of screen, checks if stash open, breaks if it is.
+# if not, looks for text for stash, if found clicks on stash moving character
+# to its location. screencaps again to ensure its open.
