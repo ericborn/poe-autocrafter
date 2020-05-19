b42e7c12d68c7bf7e320cbd49f50433507db6273
Eric Born <eric.born85@gmail.com>
2020-05-18 21:35:47 -0700

    Update find_tab.py

--- find_tab.py
+++ find_tab.py
@@ -26,30 +26,45 @@ right_arrow_coords = (609,130, 626,156)
 ## brown arrow colors
 #brown_arrow_colors = (55,23,1), (254,237,184), (81,38,0)
 
+# grey arrow colors1
 grey_arrow_value = (31,31,31)
-brown_arrow_value = (55,23,1)
+#brown_arrow_value = (55,23,1)
 
 
 # screenshot left and right arrows
-left_arrow_img = screenshot(left_arrow_coords)
-right_arrow_img = screenshot(right_arrow_coords)
+#left_arrow_img = screenshot(left_arrow_coords)
+#right_arrow_img = screenshot(right_arrow_coords)
+#
+#left_arrow_pixels = left_arrow_img.load()
+#right_arrow_pixels = right_arrow_img.load()
+#
+#left = 0
+#right = 0
+#for i in range(left_arrow_img.size[0]):
+#    for j in range(left_arrow_img.size[1]):
+#        if left_arrow_pixels[i,j] == grey_arrow_value:
+#            left = 0
+#            right = 1
 
-left_arrow_pixels = left_arrow_img.load()
-right_arrow_pixels = right_arrow_img.load()
-
-left = 0
-right = 0
-for i in range(left_arrow_img.size[0]):
-    for j in range(left_arrow_img.size[1]):
-        if left_arrow_pixels[i,j] == grey_arrow_value:
-            left = 0
-            right = 1
+#!!!TODO!!!
+#CHECKING FOR BOTH ARROWS BEING BROWN, DOES IT MATTER OR JUST SCROLL LEFT 
+#UNTIL GREY THEN START SCANNING FOR DESIRED TABS?
 
 
-def check_arrow_grey(img):
+# creates a function that checks if the area screenshotted has grey pixels
+# grey indicates the stash cannot be scrolled any further in that direction
+# 0 means grey, no scroll, 1 means brown and can be scrolled
+def check_arrow_grey(coords):
     left = 0
     right = 0
+    # take screenshot based upon coords inputting
+    img = screenshot(coords)
+    
+    # convert image to pixel map
     img_pixels = img.load()
+    
+    # check pixel map for gray values indicating 
+    #stash cannot be scrolled that way any further
     for i in range(img.size[0]):
         for j in range(img.size[1]):
             if img_pixels[i,j] == grey_arrow_value:
@@ -60,4 +75,4 @@ def check_arrow_grey(img):
                 right = 0
     return(left, right)
     
-check_arrow_grey(right_arrow_img)
+check_arrow_grey(right_arrow_coords)
