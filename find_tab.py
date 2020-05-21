0bc58535f438c5709bfa83e32138914ea8614897
Eric Born <eric.born85@gmail.com>
2020-05-21 15:54:50 -0700

    building out find_tab

--- find_tab.py
+++ find_tab.py
@@ -8,8 +8,10 @@ Used to find a particular tab within the inventory
 """
 from image_manip import color_text, image_adjustments, screenshot
 from constants import LEFT_ARROW_COORDS, RIGHT_ARROW_COORDS, GREY_ARROW_COLOR,\
-                      ITEM_IN_STASH_COORDS, TOP_LEFT_INVENTORY_COORDS
+                      ITEM_IN_STASH_COORDS, TOP_LEFT_INVENTORY_COORDS,\
+                      TOP_LEFT_CORNER, STASH_TAB_COORDS
 import pyautogui as gui
+from checks import check_inv_stash, check_for_mod
 
 #!!!TODO!!!
 # create function that takes tab name as input. Clicks tab scroll button to
@@ -17,17 +19,47 @@ import pyautogui as gui
 # not found clicks right button x times and screenshots, parses text. Continues
 # until tab is found
 
-#!!!TODO!!!
-#CHECKING FOR BOTH ARROWS BEING BROWN, DOES IT MATTER OR JUST SCROLL LEFT 
-#UNTIL GREY THEN START SCANNING FOR DESIRED TABS?
+# checks for the desired mod on the item being rolled
+def check_for_text(mod):
+    gui.moveTo(ITEM_IN_STASH_COORDS)
+    
+    # sleep 0.1 second to allow item text to appear
+    t.sleep(0.1)
+
+    img = screenshot(ENTIRE_STASH_COORDS)
+    img = color_text(img, BLUE_COLOR)
+    img = image_adjustments(img)
+    
+    parsed_text = []
+    for i in range(len(img)):
+        parsed_text.append(pytesseract.image_to_string(img[i], lang='eng', 
+                                                  config = '--psm 12').lower())
+        
+    mod_found = 0
+    #print(mod)
+    for i in range(len(parsed_text)):      
+        if bool(re.search(mod, parsed_text[i])):
+            mod_found += 1
+    # if mod found is greater than 0, mod is found so return -1 to stop rolling
+    # mod found = 0, return 1 which indicates continue to roll
+    if mod_found > 0:
+        return(-1)
+    else:
+        return(1)   
+
+check_for_mod('$$$')
 
 
-# creates a function that checks if the area screenshotted has grey pixels
+# Function that checks if the area screenshotted has grey pixels.
 # grey indicates the stash cannot be scrolled any further in that direction
 # 0 means grey, no scroll, 1 means brown and can be scrolled
+# coords input indicates the arrow to check
 def check_stash_arrows(coords):
+    # moves cursor to ensure its out of screenshot area
+    gui.moveTo(TOP_LEFT_CORNER)
+    
     grey = 0
-    #right = 0
+
     # take screenshot based upon coords inputting
     img = screenshot(coords)
     
@@ -46,20 +78,13 @@ def check_stash_arrows(coords):
     
     
 
-                    
-    # pick up currency for rolling
-    gui.rightClick()
-    
-    # move mouse to item location in stash tab 355, 766
-    gui.moveTo(ITEM_IN_STASH_COORDS)
-    
-    # roll item
-    gui.leftClick()
-
-def click_arrow(direction):
-    # move mouse to arrow in stash
-    gui.moveTo(TOP_LEFT_INVENTORY_COORDS)
-    
+def scroll_stash(direction):
+    if check_inv_stash() == 1 and check_stash_arrows(direction) == 1:
+        # move mouse to arrow in stash
+        gui.moveTo(direction)
+        for i in range(5):
+            # roll item
+            gui.leftClick()
     
     
     
