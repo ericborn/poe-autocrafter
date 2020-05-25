b86ed46c10e511569950ce4ffb0bc48200b77a19
Eric Born <eric.born85@gmail.com>
2020-05-25 12:29:21 -0700

    Update find_tab.py

--- find_tab.py
+++ find_tab.py
@@ -7,6 +7,7 @@ Created on Mon May 18 12:32:31 2020
 Used to find a particular tab within the inventory
 """
 import re
+import math as m
 import pyautogui as gui
 import pytesseract
 from checks import check_inv_stash, check_for_mod
@@ -18,9 +19,11 @@ from constants import LEFT_ARROW_COORDS, RIGHT_ARROW_COORDS, GREY_ARROW_COLOR,\
                       LEFT_ARROW_CLICK_COORDS, YELLOW_TEXT, TAB_CLICK_COORDS,\
                       TAB_BOARDER_COORDS, RIGHT_ARROW_CLICK_COORDS,\
                       STASH_SEARCH_BOX, SORT_SEARCH_NAMES, YELLOW_BORDER,\
-                      ENTIRE_STASH_COORDS
+                      ENTIRE_STASH_COORDS, INVENTORY_Y_COORDS, \
+                      INVENTORY_X_COORDS
 
 def check_stash_tabs():
+    gui.moveTo(TOP_LEFT_CORNER)
     parsed_text = []
     for i in range(len(TAB_BOARDER_COORDS)):
         img = screenshot(TAB_BOARDER_COORDS[i])
@@ -31,11 +34,9 @@ def check_stash_tabs():
             parsed_text.append(pytesseract.image_to_string(img[j], lang='eng', 
                                                       config = '--psm 12').lower())
     return(parsed_text)
-    
-check_stash_tabs()
 
 def check_for_text(text, coords):
-    #gui.moveTo(ITEM_IN_STASH_COORDS)
+    gui.moveTo(TOP_LEFT_CORNER)
     
     img = screenshot(coords)
     img = color_text(img, STASH_BLACK_TEXT, BLACK_TEXT)
@@ -99,15 +100,10 @@ def click_on_tab(keyword):
             break
     return(click_check)
 
-
-click_on_tab('splt')
-
-def move_to_inventory():
+def item_to_inventory():
     gui.keyDown('ctrl')
     gui.leftClick()
     gui.keyUp('ctrl')
-
-move_to_inventory()
 
 # moves to search box location, clicks to start the cursor, 
 # types the search word
@@ -117,27 +113,7 @@ def stash_search(keyword):
     gui.hotkey('ctrl', 'a')
     gui.hotkey('backspace')
     gui.typewrite(keyword)
-
-stash_search('map')
-
-# take screenshot based upon coords inputting
-img = screenshot(LEFT_ARROW_COORDS)
-
-# convert image to pixel map
-img_pixels = img.load()
-
-grey = 0
-
-# check pixel map for gray values indicating 
-#stash cannot be scrolled that way any further
-for i in range(img.size[0]):
-    for j in range(img.size[1]):
-        if img_pixels[i,j] == GREY_ARROW_COLOR:
-            grey += 1
-            break
-print(grey)
-
-
+    
 # Function that checks if the area screenshotted has grey pixels.
 # grey indicates the stash cannot be scrolled any further in that direction
 # 0 means grey, no scroll, 1 means brown and can be scrolled
@@ -170,6 +146,95 @@ def scroll_stash(direction):
     for i in range(7):
         # left click
         gui.leftClick()
+
+
+# find and click on a dump tab
+click_on_tab('dump')
+
+# search for the first item from the SORT_SEARCH_NAMES list
+stash_search(SORT_SEARCH_NAMES[0])
+
+total_items = 0
+
+for i in range(60):
+    # screenshot the stash
+    stash_shot = screenshot(ENTIRE_STASH_COORDS)
+    
+    # convert image to pixel map
+    stash_shot_pixels = stash_shot.load()
+    
+    # create empty list to store item yellow border coordinates
+    pixel_list = []
+    
+    for k in range(stash_shot.size[0]):
+        for l in range(stash_shot.size[1]):
+            if stash_shot_pixels[k,l] == YELLOW_BORDER:
+                pixel_list.append([k,l])
+                break
+
+    if len(pixel_list) == 0:
+        print('none found')
+        break
+    else:
+        gui.moveTo([pixel_list[0][0] + 25, pixel_list[0][1] + 170])
+        total_items += 1
+        item_to_inventory()
+
+# search for splinter tab
+# if the click_on_tab returns 0, the tab name wasnt found
+# check to see if the left scroll arrow is grey, if it is, scroll right
+for i in range(6):
+    tab_check = click_on_tab(STASH_TAB_NAMES[0])
+
+    if tab_check == 0:
+        #if check_stash_arrows(LEFT_ARROW_COORDS) > 0:
+        scroll_stash(RIGHT_ARROW_CLICK_COORDS)
+    else:
+        print('found it')
+        break
+
+# divide the total_items by 5, which is number of items in the inventory column
+# uses ceiling from math to round up which results in the total number of 
+# columns needed to click through
+
+m.ceil(total_items / 5)
+for j in range(m.ceil(total_items / 5)):
+    for i in range(total_items):
+        #print(i, j)
+        print([INVENTORY_Y_COORDS[i], INVENTORY_X_COORDS[j]])
+        
+        gui.moveTo([INVENTORY_Y_COORDS[i], INVENTORY_X_COORDS[j])
+        item_to_inventory()
+
+
+# search for the first item from the SORT_SEARCH_NAMES list
+stash_search(SORT_SEARCH_NAMES[0])
+
+
+
+
+
+
+
+# take screenshot based upon coords inputting
+img = screenshot(LEFT_ARROW_COORDS)
+
+# convert image to pixel map
+img_pixels = img.load()
+
+grey = 0
+
+# check pixel map for gray values indicating 
+#stash cannot be scrolled that way any further
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        if img_pixels[i,j] == GREY_ARROW_COLOR:
+            grey += 1
+            break
+print(grey)
+
+
+
 
 
 
@@ -211,7 +276,9 @@ for i in range(60):
 # counts to 60, influenced counts to 6. 60 item slots in inventory.
 # NEEDS TO CHECK FOR THE ITEM AGAIN AFTER MOVING TO INVENTORY AND BACK TO STASH
 # UNLESS LIST WAS ALREADY AT 0 IN PREVIOUS CHECK
-            
+       
+# NEEDS TO REVERSE THE NUMBER OF CLICKS IT MOVED TO FIND THE TAB TO DROP IN    
+    
 for i in range(60):
     #for j in range(len(SORT_SEARCH_NAMES)):
     for j in range(1):
