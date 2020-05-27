0e75fd39bf5f74089c6cb9ffe1a42fc1a72c3ef7
Eric Born <eric.born85@gmail.com>
2020-05-27 09:14:36 -0700

    Create resonator.py

---
+++ resonator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 08:57:16 2020
+
+@author: Eric
+"""
+
+FOSSIL_NAMES = ['jagged', 'dense', 'frigid', 'aberrant', 'scorched', 
+                'metallic', 'pristine', 'bound', 'corroded', 'perfect', 
+                'prismatic', 'enchanted', 'aetheric', 'lucent', 'serrated',
+                'shuddering', 'tangled', 'bloodstained','gilded', 'encrusted', 
+                'sanctified', 'hollow', 'fractured', 'glyphic', 'faceted']
+RESSONATORS = ['primitive alchemical', 'primitive chaotic', 
+               'potent alchemical', 'potent chaotic', 'powerful alchemical',
+               'powerful chaotic', 'prime alchemical', 'prime chaotic']
+
+FOSSIL_LOCATIONS = []
