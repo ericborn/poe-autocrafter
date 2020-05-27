a24b4d5b9c1635dca9b46db72e1477e8141d7bee
Eric Born <eric.born85@gmail.com>
2020-05-27 10:30:37 -0700

    Update resonator.py

--- resonator.py
+++ resonator.py
@@ -4,18 +4,20 @@ Created on Wed May 27 08:57:16 2020
 
 @author: Eric
 """
+import time as t
+import pyautogui as gui
+from constants import FOSSIL_DICT, RESSONATOR_DICT
+                     #FOSSIL_NAMES, FOSSIL_COORDS, RESSONATORS,\ 
+                     #RESONNATOR_COORDS
+                      
 
-FOSSIL_NAMES = ['jagged', 'dense', 'frigid', 'aberrant', 'scorched', 
-                'metallic', 'pristine', 'bound', 'corroded', 'perfect', 
-                'prismatic', 'enchanted', 'aetheric', 'lucent', 'serrated',
-                'shuddering', 'tangled', 'bloodstained','gilded', 'encrusted', 
-                'sanctified', 'hollow', 'fractured', 'glyphic', 'faceted']
-RESSONATORS = ['primitive alchemical', 'primitive chaotic', 
-               'potent alchemical', 'potent chaotic', 'powerful alchemical',
-               'powerful chaotic', 'prime alchemical', 'prime chaotic']
+for i in FOSSIL_DICT:
+    gui.moveTo(FOSSIL_DICT[i])
+    t.sleep(1)
 
-FOSSIL_LOCATIONS = [(129,236), (197,236), (266,236), (333,236), (398,236),
-                    (467,236), (534,236), (66,300), (129,300), (197,300),
-                    (266,300), (333,300), (398,300), (467,300), (534,300),
-                    (600,300), (101,371), (168,371), (266,371), (370,371),
-                    (398,371), (501,371), (564,371), (129,435), (534,435)]
+FOSSIL_DICT[0]
+
+
+def resonator_unstack(number, res_type):
+    gui.moveTo(RESONNATOR_COORDS[res_type])
+    
