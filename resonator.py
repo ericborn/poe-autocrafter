d02507132f23da0e5eb9bde1337bae25422198a9
Eric Born <eric.born85@gmail.com>
2020-05-27 09:33:56 -0700

    Update resonator.py

--- resonator.py
+++ resonator.py
@@ -14,4 +14,8 @@ RESSONATORS = ['primitive alchemical', 'primitive chaotic',
                'potent alchemical', 'potent chaotic', 'powerful alchemical',
                'powerful chaotic', 'prime alchemical', 'prime chaotic']
 
-FOSSIL_LOCATIONS = []
+FOSSIL_LOCATIONS = [(129,236), (197,236), (266,236), (333,236), (398,236),
+                    (467,236), (534,236), (66,300), (129,300), (197,300),
+                    (266,300), (333,300), (398,300), (467,300), (534,300),
+                    (600,300), (101,371), (168,371), (266,371), (370,371),
+                    (398,371), (501,371), (564,371), (129,435), (534,435)]
